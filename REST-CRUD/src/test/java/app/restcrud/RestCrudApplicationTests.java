d9eb77f0a4392fafff45ac510524bb15fb6840bf
NorbertWoj <norbert.wojcik.it@gmail.com>
2023-06-08 20:46:07 +0200

    Added REST-CRUD

---
+++ REST-CRUD/src/test/java/app/restcrud/RestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package app.restcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
