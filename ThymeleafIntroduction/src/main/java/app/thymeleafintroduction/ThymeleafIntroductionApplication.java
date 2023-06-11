646d6648350764c560b2c92fdb53801702a0a5ad
NorbertWoj <norbert.wojcik.it@gmail.com>
2023-06-11 11:51:06 +0200

    Added ThymeleafIntroduction

---
+++ ThymeleafIntroduction/src/main/java/app/thymeleafintroduction/ThymeleafIntroductionApplication.java
@@ -0,0 +1,13 @@
+package app.thymeleafintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafIntroductionApplication.class, args);
+    }
+
+}
